1259b411efb89333d3762db04df8c2d9c61f887d
arnau-m <arnau.arnau06@gmail.com>
2020-05-24 19:17:18 +0200

    Session form

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-notes = []
-
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if session.get("notes") is None:
+        session["notes"] = []
     if request.method == "POST":
         note = request.form.get("note")
-        notes.append(note)
-    return render_template("index.html", notes=notes)
+        session["notes"].append(note)
+    return render_template("index.html", notes=session["notes"])
