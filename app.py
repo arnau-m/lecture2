33f653c855621f30b6d95eb30b1bcdf5e14e4a6d
arnau-m <arnau.arnau06@gmail.com>
2020-05-24 18:19:00 +0200

    Dynamic Forms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/more")
-def more():
-    return render_template("index2.html")
+@app.route("/hello", methods=["POST"])
+def hello():
+    if request.method == "GET":
+        return "Please submit the form instead."
+    else:
+        name = request.form.get("name")
+        return render_template("hello.html", name=name)
